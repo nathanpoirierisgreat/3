6d05ce9665a275979a31958a958f44b3a65fc6c5
nathanpoirierisgreat <100322123+nathanpoirierisgreat@users.noreply.github.com>
2022-03-10 08:07:17 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,77 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 2; index++) {
+        for (let index = 0; index < 3; index++) {
+            music.playTone(330, music.beat(BeatFraction.Half))
+            music.playTone(392, music.beat(BeatFraction.Half))
+        }
+        music.playTone(440, music.beat(BeatFraction.Half))
+        music.playTone(440, music.beat(BeatFraction.Double))
+        music.playTone(392, music.beat(BeatFraction.Whole))
+        music.playTone(440, music.beat(BeatFraction.Half))
+        music.playTone(392, music.beat(BeatFraction.Half))
+        music.playTone(0, music.beat(BeatFraction.Quarter))
+        music.playTone(330, music.beat(BeatFraction.Half))
+        music.playTone(330, music.beat(BeatFraction.Half))
+        music.playTone(294, music.beat(BeatFraction.Half))
+        music.playTone(294, music.beat(BeatFraction.Half))
+        music.playTone(262, music.beat(BeatFraction.Half))
+        music.playTone(262, music.beat(BeatFraction.Half))
+        music.playTone(294, music.beat(BeatFraction.Double))
+        music.playTone(330, music.beat(BeatFraction.Whole))
+        music.playTone(294, music.beat(BeatFraction.Half))
+        music.playTone(262, music.beat(BeatFraction.Whole))
+        music.playTone(220, music.beat(BeatFraction.Whole))
+        music.playTone(0, music.beat(BeatFraction.Quarter))
+        music.playTone(523, music.beat(BeatFraction.Half))
+        music.playTone(523, music.beat(BeatFraction.Half))
+        music.playTone(523, music.beat(BeatFraction.Half))
+        music.playTone(440, music.beat(BeatFraction.Half))
+        music.playTone(440, music.beat(BeatFraction.Whole))
+        music.playTone(392, music.beat(BeatFraction.Half))
+        music.playTone(523, music.beat(BeatFraction.Half))
+        music.playTone(523, music.beat(BeatFraction.Half))
+        music.playTone(523, music.beat(BeatFraction.Half))
+        music.playTone(440, music.beat(BeatFraction.Whole))
+        music.playTone(392, music.beat(BeatFraction.Half))
+        music.playTone(392, music.beat(BeatFraction.Half))
+        music.playTone(330, music.beat(BeatFraction.Half))
+        music.playTone(392, music.beat(BeatFraction.Whole))
+        music.playTone(392, music.beat(BeatFraction.Half))
+        music.playTone(330, music.beat(BeatFraction.Half))
+        music.playTone(392, music.beat(BeatFraction.Whole))
+        music.playTone(440, music.beat(BeatFraction.Whole))
+    }
+    music.playTone(392, music.beat(BeatFraction.Half))
+    music.playTone(330, music.beat(BeatFraction.Half))
+    music.playTone(392, music.beat(BeatFraction.Half))
+    music.playTone(330, music.beat(BeatFraction.Half))
+    music.playTone(392, music.beat(BeatFraction.Half))
+    music.playTone(440, music.beat(BeatFraction.Half))
+    music.playTone(440, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Half))
+    music.playTone(392, music.beat(BeatFraction.Double))
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 37; index++) {
+        basic.showLeds(`
+            . . . . .
+            # . # . #
+            . # # # .
+            . . # . .
+            . # . # .
+            `)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            . # # # .
+            . . # . .
+            . # . # .
+            `)
+    }
+    basic.clearScreen()
+})
+basic.showString("Hello Mr Lachance. press A and on the 8th note, press B.")
+basic.forever(function () {
+	
+})
